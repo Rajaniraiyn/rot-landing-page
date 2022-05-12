3fd8c3cd74104c2af0c28b68521f05c71435cf34
Rajaniraiyn <rajaniraiyn@gmail.com>
2022-05-12 12:17:28 +0530

    ⬆️ Improve About section

--- src/components/AboutBanner.svelte
+++ src/components/AboutBanner.svelte
@@ -1,46 +1,67 @@
 <script lang="ts">
-    import Line from "../assets/line.png";
+    import Button from "./Button.svelte";
     export let title;
     export let subtitle;
     export let content;
     export let learnMoreLink;
     export let image;
+    export let alternate;
 </script>
 
-<div>
-    <article>
-        <h2>{title}</h2>
-        <h3>{subtitle}</h3>
-        <p>{content}</p>
-        <a href={learnMoreLink}>Learn More..</a>
-    </article>
-    <img src={image} alt="" />
+<div class:alternate>
+    {#if alternate}
+        <img src={image} alt="" />
+        <article>
+            <h2>{title}</h2>
+            <h3>{subtitle}</h3>
+            <p>{content}</p>
+            <a href={learnMoreLink}>
+                <Button>Learn More..</Button>
+            </a>
+        </article>
+    {:else}
+        <article>
+            <h2>{title}</h2>
+            <h3>{subtitle}</h3>
+            <p>{content}</p>
+            <a href={learnMoreLink}>
+                <Button>Learn More..</Button>
+            </a>
+        </article>
+        <img src={image} alt="" />
+    {/if}
 </div>
 
 <style>
     div {
         display: grid;
-        grid-template-columns: 1fr 1fr 1fr;
+        grid-template-columns: 1fr 1fr;
         gap: 1.5rem;
+        justify-items: center;
+        align-items: center;
     }
+
     article {
         border-left: solid 0.25rem #ffffff;
-        border-radius: 5px;
+        padding-left: 2.5em;
     }
 
     img {
-        width: 50%;
+        padding-inline: 5em;
     }
+
     h3,
     h2 {
         font-size: 1.5em;
         font-weight: bold;
         margin-bottom: 1rem;
     }
+
     p {
         font-size: 1em;
         margin-bottom: 1rem;
     }
+
     a {
         font-size: 1em;
         font-weight: bold;
--- src/components/AboutSection.svelte
+++ src/components/AboutSection.svelte
@@ -17,16 +17,31 @@
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae quam sit scelerisque enim in sed diam nec, blandit. Amet tellus massa sed iaculis. Mauris, curabitur scelerisque sagittis posuere mollis. In dolor neque et vitae lectus vulputate pellentesque luctus.",
             learnMoreLink: "#",
             image: EsportsPic,
+            alternate: true,
         },
     ];
 </script>
 
 <div>
     {#each about as item}
-        {@const { title, subtitle, content, learnMoreLink, image } = item}
-        <AboutBanner {title} {subtitle} {content} {learnMoreLink} {image} />
+        {@const { title, subtitle, content, learnMoreLink, image, alternate } =
+            item}
+        <AboutBanner
+            {alternate}
+            {title}
+            {subtitle}
+            {content}
+            {learnMoreLink}
+            {image}
+        />
     {/each}
 </div>
 
 <style>
+    div {
+        display: grid;
+        grid-template-rows: 1fr 1fr;
+        gap: 2em;
+        margin-inline: 5em;
+    }
 </style>
