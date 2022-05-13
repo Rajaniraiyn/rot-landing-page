f16fbe3fe00bededa77a618db88feb95404583f1
Rajaniraiyn <rajaniraiyn@gmail.com>
2022-05-13 09:12:04 +0530

    🚀 Improve performance

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { ViteWebfontDownload } from 'vite-plugin-webfont-dl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [svelte()]
+  plugins: [
+    svelte(),
+    ViteWebfontDownload()
+  ]
 })
