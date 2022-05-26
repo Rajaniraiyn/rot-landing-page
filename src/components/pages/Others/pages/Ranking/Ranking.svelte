9aca2ae9e76b16ff9106e8ea7c3a9ec5251c3ff6
siyam <siyamsundar475@gmail.com>
2022-05-26 21:30:12 +0530

    📑tabular column

---
+++ src/components/pages/Others/pages/Ranking/Ranking.svelte
@@ -0,0 +1,105 @@
+<script lang="ts"></script>
+
+<div>
+    <table class="table">
+        <tr>
+            <th>RANK</th>
+            <th>NAME</th>
+            <th>COUNTRY</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>jenin</td>
+            <td>IND</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>jenin</td>
+            <td>IND</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>jenin</td>
+            <td>IND</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>jenin</td>
+            <td>IND</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>jenin</td>
+            <td>IND</td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td>jenin</td>
+            <td>IND</td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>jenin</td>
+            <td>IND</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>jenin</td>
+            <td>IND</td>
+        </tr>
+        <tr>
+            <td>9</td>
+            <td>jenin</td>
+            <td>IND</td>
+        </tr>
+        <tr class="you">
+            <td>10</td>
+            <td>RAJ</td>
+            <td>IND</td>
+        </tr>
+    </table>
+</div>
+
+<style>
+    div{
+        padding: 2.5em;
+    }
+    .table {
+        border-collapse: collapse;
+        margin: 30px 0;
+        font-size: 0.9em;
+        min-width: 100%;
+        border-radius: 5px 5px 0 0;
+        overflow: hidden;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+    }
+
+    td, th {
+        border-bottom: 1px solid #dddddd;
+        text-align: left;
+        padding: 14px;
+        inset: 0;
+    }
+
+    th{
+        background: rgb(82, 11, 99);
+    }
+
+    .you{
+        color: #fff;
+        text-shadow:
+            0 0 7px #fff,
+            0 0 10px #fff,
+            0 0 21px #fff,
+            0 0 42px rgb(136, 0, 255),
+            0 0 82px rgb(136, 0, 255),
+            0 0 92px rgb(136, 0, 255),
+            0 0 102px rgb(136, 0, 255),
+            0 0 151px rgb(136, 0, 255);
+    }
+
+    .table tr:last-of-type{
+        border-bottom: 2px solid rgb(82, 11, 99);
+    }
+
+</style>
