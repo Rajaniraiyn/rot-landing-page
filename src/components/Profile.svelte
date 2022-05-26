7c36c7237c3efbde2ff21ea9ac9f783fe6041fca
siyam <siyamsundar475@gmail.com>
2022-05-26 11:39:10 +0530

    ⏹️ tournments details

--- src/components/Profile.svelte
+++ src/components/Profile.svelte
@@ -1,26 +1,30 @@
 <script lang="ts">
     import Bg from "../assets/bg.png";
-    import Message from "../assets/message.png"
-    import Bell from "../assets/bell.png"
-    import valo from "../assets/valo.png"
-    import pes from "../assets/pes.png"
-    import cod from "../assets/COD.png"
+    import Message from "../assets/message.png";
+    import Bell from "../assets/bell.png";
+    import valo from "../assets/valo.png";
+    import pes from "../assets/pes.png";
+    import cod from "../assets/COD.png";
 </script>
 
 <div class="main">
     <div class="head">
-        <a  href="/" class="logo">ROT</a>
+        <a href="/" class="logo">ROT</a>
         <div class="search">
-            <input type="search" placeholder="  search..." class="roundsearch"/>
+            <input
+                type="search"
+                placeholder="  search..."
+                class="roundsearch"
+            />
         </div>
-        <span class="circle"></span>
-        <img src={Message} alt="message" class="mess">
-        <img src={Bell} alt="bell" class="bell">
+        <span class="circle" />
+        <img src={Message} alt="message" class="mess" />
+        <img src={Bell} alt="bell" class="bell" />
     </div>
     <aside>
         <ul>
             <li><a href="">HOME</a></li>
-            <li><a href="">RANKING</a> </li>
+            <li><a href="">RANKING</a></li>
             <li><a href="">PROFILE</a></li>
             <li><a href="">TEAM</a></li>
             <li><a href="">TOURNAMENTS</a></li>
@@ -39,25 +43,43 @@
             <h2>UPCOMING TOURNMENTS</h2>
             <div class="tournments">
                 <div class="games">
-                    <h3>Valorant</h3> 
-                    <img src={valo} alt="valorant" class="gameimg">
+                    <img src={valo} alt="valorant" class="gameimg" />
+                    <div class="overlay">
+                        <h3>Valorant</h3>
+                        <span class="date">DATE - 22/7/22</span>
+                        <span class="time" >TIME - 3.45AM</span>
+                        <span class="region" >REGION - IND</span>
+                        <a href="" />
+                    </div>
                 </div>
                 <div class="games">
-                    <h3>PES</h3> 
-                    <img src={pes} alt="pes" class="gameimg">
+                    <img src={pes} alt="pes" class="gameimg" />
+                    <div class="overlay">
+                        <h3>PES</h3>
+                        <span class="date">DATE - 22/7/22</span>
+                        <span class="time" >TIME - 3.45AM</span>
+                        <span class="region" >REGION - IND</span>
+                        <a href="" />
+                    </div>
                 </div>
                 <div class="games">
-                    <h3>Call of Duty</h3> 
-                    <img src={cod} alt="COD" class="gameimg">
+                    <img src={cod} alt="COD" class="gameimg" />
+                    <div class="overlay">
+                        <h3>Call of Duty</h3>
+                        <span class="date">DATE - 22/7/22</span>
+                        <span class="time" >TIME - 3.45AM</span>
+                        <span class="region" >REGION - IND</span>
+                        <a href="" />
+                    </div>
                 </div>
             </div>
         </div>
-    </div> 
+    </div>
 </div>
 <img src={Bg} alt="background" id="bg" />
 
 <style>
-     .main{
+    .main {
         display: grid;
         grid-template-columns: 260px 1fr;
         grid-template-rows: 50px 1fr;
@@ -67,49 +89,49 @@
         position: fixed;
     }
 
-    .head{
+    .head {
         display: flex;
         background: black;
         width: 100vw;
-        height: 50px; 
+        height: 50px;
         align-items: center;
         grid-column: span 2;
     }
 
-    .logo{
+    .logo {
         font-size: 1.5em;
         align-items: initial;
         margin-left: 1.5em;
     }
 
-    .search{
+    .search {
         margin-left: 3em;
     }
 
-    .roundsearch{ 
+    .roundsearch {
         border-radius: 15px;
         border: 1px #000 solid;
     }
 
-    .mess{
+    .mess {
         height: 23px;
         width: 23px;
         margin-left: 55em;
     }
 
-    .bell{
+    .bell {
         height: 23px;
         width: 23px;
         margin-left: 2em;
     }
 
-    #bg{
+    #bg {
         position: absolute;
         inset: 0;
-        z-index: -9999
-        }
+        z-index: -9999;
+    }
 
-    aside{
+    aside {
         background: black;
         width: 250px;
     }
@@ -123,49 +145,72 @@
         align-items: center;
         margin: 0;
         padding: 0;
-        height:93.5vh; 
+        height: 93.5vh;
     }
 
-    a{
+    a {
         color: white;
         text-decoration: none;
     }
 
-    a::after{
-    content: '';
-    display: block;
-    width: 0;
-    height: 2px;
-    background:white ;
+    a::after {
+        content: "";
+        display: block;
+        width: 0;
+        height: 2px;
+        background: white;
     }
 
-    a:hover::after{
+    a:hover::after {
         width: 100%;
         transition: width 500ms;
     }
 
-    .display{
+    .display {
         display: grid;
     }
-    .tournments{
+    .tournments {
         display: grid;
-        grid-template-columns: repeat(3,1fr);
+        grid-template-columns: repeat(3, 1fr);
         gap: 1.5em;
         height: 30vh;
         padding: 2.5em;
     }
 
-    .games{
-        background: black;
-        border-radius: 10px
+    .games {
+        text-align: center;
+        padding: 1em;
+        position: relative;
     }
 
-    .gameimg{
-        opacity: 0.4;
+    .games .overlay {
+        position: absolute;
+        inset: 0;
+        z-index: -99;
+        background: linear-gradient(
+            180deg,
+            rgba(0, 0, 0, 1) 0%,
+            rgba(0, 0, 0, 0) 100%
+        );
+        border-radius: 10px;
+        display: grid;
+        gap: 0em;
     }
 
-    h3{
+    .gameimg {
+        position: absolute;
+        inset: 0;
+        z-index: -999;
+        border-radius: 10px;
+    }
+
+    .time, .date, .region{
+        text-align: left;
+        margin-left: 1em;
+    }
+
+    h3 {
+        justify-content: center;
         font-weight: bold;
     }
-    
 </style>
